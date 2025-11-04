ddbd4eda4c5adf644bb280534e129ff744eeb8ee
Raphael Stolt <raphael.stolt@gmail.com>
2025-11-04 08:46:59 +0100

    ci: configures the PHP CS Fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,28 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$finder = Finder::create()
+    ->in([__DIR__, __DIR__ . DIRECTORY_SEPARATOR . 'tests']);
+
+$rules = [
+    'psr_autoloading' => false,
+    '@PSR2' => true,
+    'phpdoc_order' => true,
+    'ordered_imports' => true,
+    'native_function_invocation' => [
+        'include' => ['@internal'],
+        'exclude' => ['file_put_contents']
+    ]
+];
+
+$cacheDir = \getenv('HOME') ? \getenv('HOME') : __DIR__;
+
+$config = new Config();
+
+return $config->setRules($rules)
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setFinder($finder)
+    ->setCacheFile($cacheDir . '/.php-cs-fixer.cache');
